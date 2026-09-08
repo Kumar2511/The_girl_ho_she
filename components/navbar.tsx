231950e2a4dd4c06d7dbd21398ace6d7f9284791
Senthil Kumar <senthil.s111002@gmail.com>
2026-09-08 15:21:28 +0530

    Updated logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -795,7 +795,7 @@ export default function Navbar() {
   "
 >
   <img
-    src="/the-girl-house-icon-2.png"
+    src="/the-girl-house-icon.png"
     alt="The Girl House"
     className="
       block
